98729629be8dff1e2bc54a3e06960975d1ccd2e8
helenl9098 <35742422+helenl9098@users.noreply.github.com>
2019-09-02 03:22:48 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -100,7 +100,7 @@ function main() {
   // Initial call to load scene
   loadScene();
 
-  const camera = new Camera(vec3.fromValues(0, 300, -300), vec3.fromValues(0, 0, 0));
+  const camera = new Camera(vec3.fromValues(0, 45, -90), vec3.fromValues(0, 0, 0));
 
   const renderer = new OpenGLRenderer(canvas);
   renderer.setClearColor(0 / 255.0, 0.0 / 255.0, 1.0, 1);
